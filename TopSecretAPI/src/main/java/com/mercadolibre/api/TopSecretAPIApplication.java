acf90a39de903b75d831faee1ecf5729b6dcd744
Facundo Mediotte <facumediotte@gmail.com>
2021-02-07 18:59:43 -0300

    Swagger documentation

--- TopSecretAPI/src/main/java/com/mercadolibre/api/TopSecretAPIApplication.java
+++ TopSecretAPI/src/main/java/com/mercadolibre/api/TopSecretAPIApplication.java
@@ -2,9 +2,14 @@ package com.mercadolibre.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mercadolibre.api.swagger.SwaggerConfig;
 
 @SpringBootApplication
-public class TopSecretAPIApplication {
+public class TopSecretAPIApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopSecretAPIApplication.class, args);
